e1a0c0a8dc1a2dde8ac3aaaac09e4b65f46fa749
tm-sukehiro <k.sukehiro@tcmobile.jp>
2016-08-12 17:15:44 +0900

    react tutorial

---
+++ js/tutorial4.js
@@ -0,0 +1,19 @@
+var Comment = React.createClass({
+  rawMarkup: function() {
+    var md = new Remarkable();
+    var rawMarkup = md.render(this.props.children.toString());
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    var md = new Remarkable();
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+      </div>
+    )
+  }
+})
